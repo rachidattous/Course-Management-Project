f144b7d3fa417e127d72c60443f8d60145c2b6d3
rachid attous <rashidattous@gmail.com>
2024-01-31 10:25:49 +0100

    Removing Course Settings Repo

--- src/main/java/com/add/coursemanagement/repository/CourseSettingRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.add.coursemanagement.repository;
-//
-//import com.add.coursemanagement.model.CourseSetting;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CourseSettingRepository extends JpaRepository<CourseSetting, String> {
-//}
